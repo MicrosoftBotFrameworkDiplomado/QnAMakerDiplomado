ba325bbc936338e1774e12d2a947c08a892ee7eb
Javier Ignacio Quinteros Carvajal <javier.quinteros@inacapmail.cl>
2018-02-13 15:29:46 -0300

    Solución de tipo BOT Framework (Paquetes Nuget Agregados y Actualizados)

---
+++ DiplomadoBotsQnAMaker/Controllers/QnADialog.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiplomadoBotsQnAMaker.Controllers
+{
+    [Serializable]
+    public class QnADialog : QnAMakerDialog
+    {
+        public QnADialog () : base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnASubscriptionKey"], ConfigurationManager.AppSettings["QnAKnowledgebaseId"], "No entendí la referencia :v",0.5)))
+        {
+
+        }
+    }
+}
